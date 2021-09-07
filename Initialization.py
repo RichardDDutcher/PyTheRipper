4629809e7b4ceb6bad01be8f89da706370d5598e
Phantom9298 <53918502+Phantom9298@users.noreply.github.com>
2021-09-07 18:20:56 -0500

    subprocess test

--- Initialization.py
+++ Initialization.py
@@ -1,1 +1,7 @@
-import os
+#imports
+import subprocess as sp
+
+sp.run(["python3", "-m", "pip", "install", "PySimpleGUI"])
+sp.run(["python3", "-m", "pip", "install", "python-tk"])
+sp.run(["git", "clone", "https://github.com/Phantom9298/PyTheRipper/main/Initialization.py"])
+sp.run(["python3", "PyTheRipper.py"
