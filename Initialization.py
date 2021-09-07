0b282271e4c9b9e6d1f242cb9aad202cea13ea82
Phantom9298 <53918502+Phantom9298@users.noreply.github.com>
2021-09-07 18:21:25 -0500

    Update Initialization.py

--- Initialization.py
+++ Initialization.py
@@ -4,4 +4,4 @@ import subprocess as sp
 sp.run(["python3", "-m", "pip", "install", "PySimpleGUI"])
 sp.run(["python3", "-m", "pip", "install", "python-tk"])
 sp.run(["git", "clone", "https://github.com/Phantom9298/PyTheRipper/main/Initialization.py"])
-sp.run(["python3", "PyTheRipper.py"
+sp.run(["python3", "PyTheRipper.py"])
