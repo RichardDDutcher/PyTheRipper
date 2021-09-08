890f0cfe4116f2ad679339caa35dfc1311367386
Phantom9298 <53918502+Phantom9298@users.noreply.github.com>
2021-09-08 08:01:15 -0500

    Update Initialization.py

--- Initialization.py
+++ Initialization.py
@@ -5,3 +5,4 @@ sp.run(["python3", "-m", "pip", "install", "PySimpleGUI"])
 sp.run(["sudo", "apt-get", "install", "python3-tk"])
 sp.run(["git", "clone", "https://github.com/Phantom9298/PyTheRipper/main/Initialization.py">
 sp.run(["python3", "PyTheRipper.py"])
+sp.run(["clear"])
