da29816863ad77eb3558a862b9a095c1a52e5f63
Phantom9298 <53918502+Phantom9298@users.noreply.github.com>
2021-09-09 08:43:20 -0500

    Update PyTheRipper.py

--- PyTheRipper.py
+++ PyTheRipper.py
@@ -4,20 +4,13 @@ import tk
 import os.path
 
 #Global Variables
+frame = frame(self, borderwidth=10)
+frame.pack()
 title = "PyTheRipper"
-layout = [[sg.Text("Welcome To PyTheRipper")],[,[sg.Button(Text="Start")],[sg.Exit]]
+layout = [[sg.Text("Welcome To PyTheRipper")],[,[sg.Button(Text="Start")],[sg.Button(Test="Cancel")]
 
 #PytheRipper GUI
 sg.Window(title, layout, margins=(100, 50)).read()
-
-frame = frame(self, borderwidth=10)
-frame.pack()
-
-btn1 = Button(frame, text="Start")
-btn1.pack(side=LEFT, padx=5)
-btn2 = Button(frame, text="Shutdown")
-btn2.pack(side=RIGHT, padx=5)
-
 
 while True:
     event, values = Window.read()
